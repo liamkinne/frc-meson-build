35d8773dd4a5fd170a3e96cccea1134b7ba9a328
liamkinne <kinneliam@gmail.com>
2018-10-03 16:38:51 +1000

    impliment in a function

--- scripts/download-wpilib.py
+++ scripts/download-wpilib.py
@@ -7,6 +7,7 @@ import wget
 import subprocess
 
 wpilib_folder = os.environ['MESON_BUILD_ROOT'] + '/wpilib/'
+plugins_page_url = 'http://first.wpi.edu/FRC/roborio/release/eclipse/plugins/'
 
 def get_full_path(name):
 	return os.environ['MESON_BUILD_ROOT'] + '/' + name 
@@ -24,20 +25,23 @@ def extract_package(name):
 	subprocess.call(['mkdir','--parents', wpilib_folder])
 	subprocess.call(['unzip', '-qo', get_full_path(name), '-d',  wpilib_folder + name])
 
-plugins_page_url = 'http://first.wpi.edu/FRC/roborio/release/eclipse/plugins/'
-plugins_page = urllib.request.urlopen(plugins_page_url);
-plugins_page_content = plugins_page.read().splitlines()
+def get_latest_version():
+	plugins_page = urllib.request.urlopen(plugins_page_url);
+	plugins_page_content = plugins_page.read().splitlines()
 
-versions = []
-for line in plugins_page_content:
-	match = re.match(r'^.*wpilib.plugins.cpp_(.*).jar.*$', str(line), re.M|re.I)
-	if match:
-		versions.append(match.group(1))
+	versions = []
+	for line in plugins_page_content:
+		match = re.match(r'^.*wpilib.plugins.cpp_(.*).jar.*$', str(line), re.M|re.I)
+		if match:
+			versions.append(match.group(1))
 
-if not versions:
-	raise Exception('Failed to parse and find versions')
+	if not versions:
+		raise Exception('Failed to parse and find any versions')
 
-version_latest = versions[-1]
+	return versions[-1]
+
+
+version_latest = get_latest_version();
 
 core_filename = 'edu.wpi.first.wpilib.plugins.core_{}.jar'.format(version_latest)
 cpp_filename = 'edu.wpi.first.wpilib.plugins.cpp_{}.jar'.format(version_latest)
